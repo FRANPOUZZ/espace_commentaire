f1198ded2af2f65a54f4ebe10fab1566f3d7d9e5
francois <francois.croizer64@gmail.com>
2024-02-09 13:25:26 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function postComment(){
+    let NewValueFirstName = document.getElementById("first-name")
+    let NewValueLastName = document.getElementById("last-name")
+    let NewValueMessage = document.getElementById("message-max")
+    document.getElementById("comment-list").appendChild(div);
+  
+  }
